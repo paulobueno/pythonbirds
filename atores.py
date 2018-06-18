4c3380d7405b68a8fbc7f3aa9ea703fab608f81f
Paulo Bueno <paulob.bruno@gmail.com>
2018-06-17 22:12:26 -0300

    Jogo funcionando

--- atores.py
+++ atores.py
@@ -124,9 +124,10 @@ class Passaro(Ator):
         :param tempo: tempo de jogo a ser calculada a posição
         :return: posição x, y
         """
-        if self.foi_lancado():
+        if self._esta_voando():
             delta_t = tempo - self._tempo_de_lancamento
             self._calcular_posicao_vertical(delta_t)
+            self._calcular_posicao_horizontal(delta_t)
         return super().calcular_posicao(tempo)
 
 
@@ -139,15 +140,23 @@ class Passaro(Ator):
         :param tempo_de_lancamento:
         :return:
         """
-        self._angulo_de_lancamento = angulo
         self._tempo_de_lancamento = tempo_de_lancamento
+        self._angulo_de_lancamento = math.radians(angulo)
 
     def _calcular_posicao_vertical(self, delta_t):
-        angulo_radianos = math.radians(self._angulo_de_lancamento)
         y_atual = self._y_inicial
-        y_atual += self.velocidade_escalar * math.sin(angulo_radianos) * delta_t
+        y_atual += self.velocidade_escalar * math.sin(self._angulo_de_lancamento) * delta_t
         y_atual -= (GRAVIDADE * delta_t ** 2) / 2
         self.y = y_atual
+
+    def _calcular_posicao_horizontal(self, delta_t):
+        x_atual = self._x_inicial
+        x_atual += self.velocidade_escalar * math.cos(self._angulo_de_lancamento) * delta_t
+        self.x = x_atual
+
+    def _esta_voando(self):
+        return self.foi_lancado() and self.status == ATIVO
+
 
 class PassaroAmarelo(Passaro):
     velocidade_escalar = 30
